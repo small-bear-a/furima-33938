2b410807a33dd6f986f649d862ffc6f38b0ce3cf
small-bear-a <77313049+small-bear-a@users.noreply.github.com>
2021-02-23 15:04:02 +0900

    商品購入機能実装

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchase_address do
+    code           { '123-4567' }
+    prefecture_id  {2}
+    city           {'練馬区'}
+    address        {1111}
+    building_name  {"マンション"}
+    phone          {12345123451}
+    # purchase_id    {2}
+    token          {"tok_abcdefghijk000000000000"}
+  end
+end
